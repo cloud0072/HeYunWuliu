0ff758435f75fbc064ae1f498ac3da3be77cf64a
caolei <352419394@qq.com>
2023-05-22 22:01:19 +0800

    build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 8001
-  },
-  plugins: [vue()],
-})
+export default ({ mode }) => {
+  const root = process.cwd();
+  const env = loadEnv(mode, root);
+
+  return {
+    base: env.VITE_APP_PUBLIC_PATH,
+    server: {
+      port: 8001
+    },
+    plugins: [vue()],
+  }
+}
