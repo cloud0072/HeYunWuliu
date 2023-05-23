29114aed5eb81137abe2c8a53898c98862d079d1
caolei <352419394@qq.com>
2023-05-23 10:19:54 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 import {createApp} from 'vue'
 import App from './App.vue'
-import {Button, Col, Form, Input, InputNumber, Row, Select, Space} from 'ant-design-vue';
+import {Button, Col, Form, Input, InputNumber, Row, Select, Space, Table} from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 
 const app = createApp(App)
 
 app.use(Button)
     .use(Form)
+    .use(Input)
+    .use(InputNumber)
     .use(Select)
     .use(Space)
+    .use(Table)
     .use(Row)
     .use(Col);
 
